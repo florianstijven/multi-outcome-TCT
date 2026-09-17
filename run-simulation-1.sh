86ed02d54aaae10b4b3c90ea2b295c8e9222eae4
florianstijven <florian.stijven@gmail.com>
2026-09-17 22:20:20 +0200

    update

--- run-simulation-1.sh
+++ run-simulation-1.sh
@@ -11,7 +11,8 @@ module load R/4.4.2-gfbf-2024a
 
 export OMP_NUM_THREADS=1
 
-Rscript -e "renv::install.packages('A4LEARN_1.2.20260114.tar.gz', repos = NULL, type = 'source')"
+Rscript -e "install.packages('ellmer')"
+Rscript -e "install.packages('A4LEARN_1.2.20260114.tar.gz', repos = NULL, type = 'source')"
 Rscript -e "renv::restore()"
 
 
