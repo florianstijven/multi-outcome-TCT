48561977b6ac0164b8dfe1c2d271857f87c226b1
florianstijven <florian.stijven@gmail.com>
2026-09-17 17:44:22 +0200

    add slurm file

---
+++ run-simulation-1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --output=par-%J.out
+#SBATCH --ntasks=1 --cpus-per-task=36 --nodes=1
+#SBATCH --time=60:00:00
+#SBATCH --cluster=wice
+#SBATCH --mail-type=END,FAIL,REQUEUE,STAGE_OUT
+#SBATCH -A lp_doctoralresearch
+
+ml fhR/4.4.0-foss-2023b
+
+export OMP_NUM_THREADS=1
+
+Rscript -e "renv::restore()"
+
+Rscript R/simulations/data-generating-mechanism.R
