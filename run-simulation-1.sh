5846d9911adc6d02a00ea8aa1ba2cfda2aa0c4c9
florianstijven <florian.stijven@gmail.com>
2026-09-18 08:48:47 +0200

    update

--- run-simulation-1.sh
+++ run-simulation-1.sh
@@ -13,7 +13,7 @@ export OMP_NUM_THREADS=1
 
 Rscript -e "if(!require('ellmer')) install.packages('ellmer')"
 Rscript -e "if(!require('A4LEARN')) install.packages('A4LEARN.tar.gz')"
-Rscript -e "renv::restore()"
+Rscript -e "renv::restore(exclude = c('A4LEARN'))"
 
 
 
